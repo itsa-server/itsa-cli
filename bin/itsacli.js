255560df2003ba3101d78e36a0a607c1748c7af8
Marco Asbreuk <marco@itsasbreuk.nl>
2015-10-12 22:24:00 +0200

    intermediate

--- bin/itsacli.js
+++ bin/itsacli.js
@@ -1,1 +1,22 @@
-console.log('ok');
+#!/usr/bin/env node
+
+'use strict';
+
+var args = process.argv,
+    action = args[2],
+    createApp = require('./create_app'),
+    validApp = /[a-zA-Z]+/,
+    subaction;
+
+if (action==='create') {
+    subaction = args[3];
+    if (subaction && subaction.test(validApp)) {
+        createApp();
+    }
+    else {
+        console.log('invalid command');
+    }
+}
+else {
+    console.log('invalid command');
+}
