20ef88905565f277f1c54dccdcddc65192c02449
Marco Asbreuk <marco@itsasbreuk.nl>
2015-10-12 20:31:34 +0200

    initial commit

---
+++ tests/itsa-cli.js
@@ -0,0 +1,14 @@
+/*global describe, it */
+/*jshint unused:false */
+
+"use strict";
+var expect = require('chai').expect,
+    should = require('chai').should();
+
+
+describe('Testgroup', function () {
+
+    it('Your test', function () {
+    });
+
+});
